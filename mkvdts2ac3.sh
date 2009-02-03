0ae4b75e9bacd9a010197c1cf5a00b165fc1452e
Jake Wharton <jakewharton@gmail.com>
2009-02-03 22:04:43 +0000

    Applied Apache 2.0 license. It applies retroactively to all source code in the repository.

--- mkvdts2ac3.sh
+++ mkvdts2ac3.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 # mkvdts2ac3 - add an ac3 track to mkv from its dts
 # Author: Jake Wharton <jakewharton@gmail.com>
-# Website: http://code.google.com/p/mkvdts2ac3/
+# Website: http://mine.jakewharton.com/projects/show/mkvdts2ac3
 # Version: 0.3.1
+# License:
+#   Copyright 2009 Jake Wharton
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 
 # Version display function
 displayversion() {
