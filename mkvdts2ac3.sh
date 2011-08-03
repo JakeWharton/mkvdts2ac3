JakeWharton/mkvdts2ac3

commit 6a5972a5b7a1fadafd2b19e45dc15c3b81101c16

Force English environment to avoid errors with mkvmerge/mkvinfo.

mkvdts2ac3.sh
@@ -48,6 +48,9 @@ if [ -f ~/.mkvdts2ac3.rc ]; then
 	. ~/.mkvdts2ac3.rc
 fi
 
+# Force English, mkvinfo may fail otherwise
+export LC_ALL=C
+
 #---------- FUNCTIONS --------
 displayhelp() {
 # Usage: displayhelp

commit 131087e7262cdbcccd8bce3c3f594d647a2b54e0

Script was failing with file paths containing non-ASCII characters.
This bug was introduced by commit [6a5972a5b7a1fadafd2b].

mkvdts2ac3.sh
@@ -48,8 +48,8 @@ if [ -f ~/.mkvdts2ac3.rc ]; then
 	. ~/.mkvdts2ac3.rc
 fi
 
-# Force English, mkvinfo may fail otherwise
-export LC_ALL=C
+# Force English output, grepping for messages may fail otherwise
+export LC_MESSAGES=C
 
 #---------- FUNCTIONS --------
 displayhelp() {